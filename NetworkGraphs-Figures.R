62f3354cf0e287dbe662853752ee09f95d3b644d
Catherine Laing <catherine.laing@york.ac.uk>
2023-09-08 11:51:48 +0100

    updated paper

--- NetworkGraphs-Figures.R
+++ NetworkGraphs-Figures.R
@@ -1,10 +1,10 @@
 path_length_plot_DT <- ggplot(data=subset(SWD_red,
                                        data_type %in% c("target", "actual")),
-                           aes(x = age, y = path_length)) +
+                           aes(x = numNodes, y = path_length)) +
   geom_smooth(size = 2, aes(fill = data_type, colour = data_type)) +
   scale_colour_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
   scale_fill_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-  xlab("Age (months)") +
+  xlab("Network size (number of nodes)") +
   ylab("Mean Path Length") +
   theme_bw()  +
   theme(text = element_text(size = 20),
@@ -24,11 +24,11 @@ path_length_plot_DT <- ggplot(data=subset(SWD_red,
 
 clust_coef_plot_DT <- ggplot(data=subset(SWD_red,
                                       data_type %in% c("target", "actual")),
-                          aes(x = age, y = clust_coef_avg)) +
+                          aes(x = numNodes, y = clust_coef_avg)) +
   geom_smooth(size = 2, aes(fill = data_type, colour = data_type)) +
   scale_colour_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
   scale_fill_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-  xlab("Age (months)") +
+  xlab("Network size (number of nodes)") +
   ylab("Clustering Coefficient") +
   theme_bw()  +
   theme(text = element_text(size = 20),
@@ -64,8 +64,8 @@ connectivity <- ggplot(data=subset(SWD_red, data_type %in% c("actual", "target")
   geom_smooth(aes(fill = data_type, colour = data_type, linetype = corpus)) +
   scale_colour_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
   scale_fill_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-  # xlab("Age (months)") +
-  # ylab("Clustering Coefficient") +
+  xlab("Network size (number of nodes)") +
+  ylab("Mean number of connections per node (mean k)") +
   theme_bw()  +
   theme(text = element_text(size = 20),
         legend.position = "bottom")
