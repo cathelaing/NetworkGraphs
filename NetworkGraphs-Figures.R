6a3b4ecf935ffbd2a324e4d55997eb82fffc8d57
Catherine Laing <catherine.laing@york.ac.uk>
2023-09-13 13:08:22 +0100

    updated paper

--- NetworkGraphs-Figures.R
+++ NetworkGraphs-Figures.R
@@ -7,20 +7,22 @@ path_length_plot_DT <- ggplot(data=subset(SWD_red,
   xlab("Network size (number of nodes)") +
   ylab("Mean Path Length") +
   theme_bw()  +
-  theme(text = element_text(size = 20),
-        legend.position = "bottom")
+  theme(text = element_text(size = 16))
 
-# path_length_plot <- ggplot(data=subset(SWD_red,
-#                                        !(data_type %in% c("target", "WS_target"))),
-#                            aes(x = age, y = path_length)) +
-#   geom_smooth(size = 2, aes(fill = data_type, colour = data_type)) +
-#  # scale_colour_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-# #  scale_fill_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-#   xlab("Age (months)") +
-#   ylab("Mean Path Length") +
-#   theme_bw()  +
-#   theme(text = element_text(size = 20),
-#         legend.position = "bottom")
+path_length_plot <- ggplot(data=subset(SWD_red,
+                                       !(data_type %in% c("target", "WS_target"))),
+                           aes(x = age, y = path_length)) +
+  geom_smooth(size = 2, aes(fill = data_type, colour = data_type)) +
+  scale_fill_discrete(name="Data type",
+                      breaks=c("actual", "Erdos_Renyi", "WS_actual"),
+                      labels=c("Real", "Simulated - random (Erdos–Rényi)", "Simulated - small world (Watts-Strogatz)")) +
+  scale_colour_discrete(name="Data type",
+                        breaks=c("actual", "Erdos_Renyi", "WS_actual"),
+                        labels=c("Real", "Simulated - random (Erdos–Rényi)", "Simulated - small world (Watts-Strogatz)")) +
+  xlab("Age (months)") +
+  ylab("Mean Path Length") +
+  theme_bw()  +
+  theme(text = element_text(size = 16))
 
 clust_coef_plot_DT <- ggplot(data=subset(SWD_red,
                                       data_type %in% c("target", "actual")),
@@ -31,20 +33,22 @@ clust_coef_plot_DT <- ggplot(data=subset(SWD_red,
   xlab("Network size (number of nodes)") +
   ylab("Clustering Coefficient") +
   theme_bw()  +
-  theme(text = element_text(size = 20),
-        legend.position = "bottom")
+  theme(text = element_text(size =16))
 
-# clust_coef_plot <- ggplot(data=subset(SWD_red,
-#                                       !(data_type %in% c("target", "WS_target"))),
-#                           aes(x = age, y = clust_coef_avg)) +
-#   geom_smooth(size = 2, aes(fill = data_type, colour = data_type)) +
-#  # scale_colour_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-# #  scale_fill_manual(name = "Data type", labels = c("Actual", "Target"), values = c("#F8766D", "#00BFC4")) + 
-#   xlab("Age (months)") +
-#   ylab("Clustering Coefficient") +
-#   theme_bw()  +
-#   theme(text = element_text(size = 20),
-#         legend.position = "bottom")
+clust_coef_plot <- ggplot(data=subset(SWD_red,
+                                      !(data_type %in% c("target", "WS_target"))),
+                          aes(x = age, y = clust_coef_avg)) +
+  geom_smooth(size = 2, aes(fill = data_type, colour = data_type)) +
+  scale_fill_discrete(name="Data type",
+                      breaks=c("actual", "Erdos_Renyi", "WS_actual"),
+                      labels=c("Real", "Simulated - random (Erdos–Rényi)", "Simulated - small world (Watts-Strogatz)")) +
+  scale_colour_discrete(name="Data type",
+                      breaks=c("actual", "Erdos_Renyi", "WS_actual"),
+                      labels=c("Real", "Simulated - random (Erdos–Rényi)", "Simulated - small world (Watts-Strogatz)")) +
+  xlab("Age (months)") +
+  ylab("Clustering Coefficient") +
+  theme_bw()  +
+  theme(text = element_text(size = 16))
 
 threshold.comp <- ggplot(data=globalthresholds,
                          aes(x = threshold, y = estimate)) +
@@ -55,7 +59,7 @@ threshold.comp <- ggplot(data=globalthresholds,
   # xlab("Age (months)") +
   # ylab("Clustering Coefficient") +
   theme_bw()  +
-  theme(text = element_text(size = 20),
+  theme(text = element_text(size = 16),
         legend.position = "bottom")
 
 connectivity <- ggplot(data=subset(SWD_red, data_type %in% c("actual", "target")),
@@ -67,7 +71,7 @@ connectivity <- ggplot(data=subset(SWD_red, data_type %in% c("actual", "target")
   xlab("Network size (number of nodes)") +
   ylab("Mean number of connections per node (mean k)") +
   theme_bw()  +
-  theme(text = element_text(size = 20),
+  theme(text = element_text(size = 16),
         legend.position = "bottom")
 
 # phonological.distance <- ggplot(comparison_summary, aes(x = age, y = dist_mean, colour = Speaker)) +
